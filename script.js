18231e9e89eade77d0e78013fc498aefcdbb11f2
Marco-Sorrentino <sorrentmarco@gmail.com>
2023-01-17 16:08:17 +0100

    js function done

---
+++ script.js
@@ -0,0 +1,12 @@
+const hideSummer = document.getElementById("summerSection");
+const hideSummerSection = document.getElementById("summerHideSection");
+const cta = document.getElementById("cta");
+const cards = document.querySelectorAll(".card");
+
+hideSummerSection.addEventListener("click", () => {
+  hideSummer.classList.toggle("hidden");
+});
+
+cta.addEventListener("click", () => {
+  alert(cards.length);
+});
